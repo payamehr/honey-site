4a039fb493125b52c090a3c478fcb7b7543e4678
payamehr <89305847+payamehr@users.noreply.github.com>
2025-08-17 12:59:20 +0330

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/honey-site/', // اگر اسم ریپو را عوض کردی، همین‌جا هم عوض کن
-})
